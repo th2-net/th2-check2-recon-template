b858229158f2862800c285b425916cb86c2ec317
Ivan Chadin <vaaaaaanya@gmail.com>
2021-03-29 19:10:50 +0400

    Update th2-check2-recon version

--- src/main.py
+++ src/main.py
@@ -17,23 +17,29 @@ import signal
 import sys
 
 from th2_check2_recon.recon import Recon
+from th2_check2_recon.services import MessageComparator
 from th2_common.schema.factory.common_factory import CommonFactory
+from th2_grpc_util.util_service import MessageComparatorService
 
 logging.config.fileConfig(fname=str(sys.argv[-1]), disable_existing_loggers=False)
 logger = logging.getLogger()
 
 factory = CommonFactory()
 grpc_router = factory.grpc_router
+event_router = factory.event_batch_router
 message_router = factory.message_parsed_batch_router
 custom_config = factory.create_custom_configuration()
-event_router = factory.event_batch_router
+message_comparator = MessageComparator(grpc_router.get_service(MessageComparatorService))
 
-recon = Recon(event_router, grpc_router, message_router, custom_config)
+recon = Recon(event_router, message_router, custom_config, message_comparator)
 
 
-def receive_signal():
-    recon.stop()
-    factory.close()
+def receive_signal(signum, frame):
+    logger.info('SIGTERM received')
+    try:
+        recon.stop()
+    finally:
+        factory.close()
 
 
 signal.signal(signal.SIGTERM, receive_signal)
@@ -41,6 +47,4 @@ signal.signal(signal.SIGTERM, receive_signal)
 try:
     recon.start()
 except KeyboardInterrupt or Exception:
-    logger.exception("Except error and try stop Recon")
-finally:
-    recon.stop()
+    pass
