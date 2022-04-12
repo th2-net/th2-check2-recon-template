0138ff702502aa0a995d284df733fa255f7fb67c
stanislav.sergeev <stanislav.sergeev@exactprosystems.com>
2022-04-12 13:54:50 +0300

    fix MarketDataRequest

--- src/rules/Match_MarketDataRequest_trades.py
+++ src/rules/Match_MarketDataRequest_trades.py
@@ -57,8 +57,8 @@ class Rule(rule.Rule):
         message_type: str = message.proto_message.metadata.message_type
         session_alias = message.proto_message.metadata.id.connection_id.session_alias
         direction = message.proto_message.metadata.id.direction
-        if session_alias not in self.config.keys() or message_type not in ['ExecutionReport',
-                                                                           'MarketDataSnapshotFullRefresh']:
+        #session_alias not in self.config.keys() or
+        if message_type not in ['ExecutionReport', 'MarketDataSnapshotFullRefresh']:
             return
         if message_type == 'ExecutionReport' and direction != Direction.FIRST:
             return
