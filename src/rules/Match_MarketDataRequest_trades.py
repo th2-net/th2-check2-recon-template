fd51b4ac450a0bd1168e4128b880c27b00b3471d
stanislav.sergeev <stanislav.sergeev@exactprosystems.com>
2022-05-16 21:31:58 +0300

    Expanded the table and refactored the code

--- src/rules/Match_MarketDataRequest_trades.py
+++ src/rules/Match_MarketDataRequest_trades.py
@@ -15,12 +15,17 @@
 import logging
 
 from th2_check2_recon import rule
-from th2_check2_recon.common import EventUtils
+from th2_check2_recon.common import EventUtils, TableComponent
 from th2_check2_recon.reconcommon import ReconMessage, MessageGroupType
-from th2_grpc_common.common_pb2 import Direction, Event
-
+from th2_grpc_common.common_pb2 import Direction, Event, EventStatus
 
 logger = logging.getLogger(__name__)
+
+
+def fill_field_value(conteiner: list, field: str, value1, value2):
+    conteiner[0][field] = value1.simple_value
+    conteiner[1][field] = value2.simple_value
+    conteiner[2][field] = "PASSED" if value1.simple_value == value2.simple_value else "FAILED"
 
 
 class Rule(rule.Rule):
@@ -84,7 +89,8 @@ class Rule(rule.Rule):
                 val += message.proto_message.fields[field_name].simple_value
         if message_type == 'MarketDataSnapshotFullRefresh':
             security_id = message.proto_message.fields['Instrument'].message_value.fields['SecurityID'].simple_value
-            security_id_source = message.proto_message.fields['Instrument'].message_value.fields['SecurityIDSource'].simple_value
+            security_id_source = message.proto_message.fields['Instrument'].message_value.fields[
+                'SecurityIDSource'].simple_value
             for field_name in ['SecurityID', 'SecurityIDSource']:
                 val += message.proto_message.fields['Instrument'].message_value.fields[field_name].simple_value
         message.hash = hash(val)
@@ -94,20 +100,74 @@ class Rule(rule.Rule):
     def check(self, messages: [ReconMessage], *args, **kwargs) -> Event:
         logger.info(f"RULE '{self.get_name()}': CHECK: input_messages: {messages}")
 
-        ignore_fields = ['CheckSum', 'header', 'trailer', 'TradingParty', 'BodyLength', 'SendingTime', 'MsgSeqNum',
-                         'CsvRecordType', 'DisplayQty', 'SecondaryClOrdID', 'TransactTime', 'MDEntryBuyer',
-                         'ExecType', 'Text', 'OrdStatus', 'CumQty', 'LeavesQty', 'ExecID', 'OrderID', 'MDEntrySeller',
-                         'MDEntryID', 'MDEntryPx', 'MDEntrySize', 'MDEntryTime', 'MDEntryType', 'Text', 'MDReqID',
-                         'Symbol', 'TrdMatchID', 'NoMDEntries', 'Instrument']
-        verification_component = self.message_comparator.compare_messages(messages, ignore_fields)
-
         info_for_name = dict()
-        for message in messages:
-            info_for_name.update(message.hash_info)
+        er = ReconMessage
+        mdr = ReconMessage
+        check = [{"SecurityID": None, "SecurityIDSource": None, "ID": None, "Price": None, "Qty": None,
+                 "Buyer": None, "Seller": None, "Time": None},
+                 {"SecurityID": None, "SecurityIDSource": None, "ID": None, "Price": None, "Qty": None,
+                 "Buyer": None, "Seller": None, "Time": None},
+                 {"SecurityID": None, "SecurityIDSource": None, "ID": None, "Price": None, "Qty": None,
+                 "Buyer": None, "Seller": None, "Time": None}]
 
-        body = EventUtils.create_event_body(verification_component)
+        msg: ReconMessage
+        for msg in messages:
+            info_for_name.update(msg.hash_info)
+            message = msg.proto_message
+
+            if message.metadata.message_type == "ExecutionReport":
+                er = message
+
+            elif message.metadata.message_type == "MarketDataSnapshotFullRefresh":
+                mdr = message
+
+        fill_field_value(check, "SecurityID", er.fields["SecurityID"],
+                         mdr.fields["Instrument"].message_value.fields["SecurityID"])
+        fill_field_value(check, "SecurityIDSource", er.fields["SecurityIDSource"],
+                         mdr.fields["Instrument"].message_value.fields["SecurityIDSource"])
+
+        value = mdr.fields["NoMDEntries"].list_value
+        if er.fields["ClOrdID"].simple_value == \
+                value.values[0].message_value.fields["MDEntryID"].simple_value:
+            value = value.values[0]
+        elif er.fields["ClOrdID"].simple_value == \
+                value.values[1].message_value.fields["MDEntryID"].simple_value:
+            value = value.values[1]
+
+        fill_field_value(check, "ID", er.fields["ClOrdID"],
+                         value.message_value.fields["MDEntryID"])
+        fill_field_value(check, "Price", er.fields["Price"],
+                         value.message_value.fields["MDEntryPx"])
+        fill_field_value(check, "Qty", er.fields["OrderQty"],
+                         value.message_value.fields["MDEntrySize"])
+
+        party = er.fields["TradingParty"].message_value.fields["NoPartyIDs"].list_value
+        for x in range(0, 4):
+            if party.values[x].message_value.fields["PartyRole"].simple_value == '76':
+                fill_field_value(check, "Buyer", party.values[x].message_value.fields["PartyID"],
+                                 value.message_value.fields["MDEntryBuyer"])
+
+            if party.values[x].message_value.fields["PartyRole"].simple_value == '17':
+                fill_field_value(check, "Seller", party.values[x].message_value.fields["PartyID"],
+                                 value.message_value.fields["MDEntrySeller"])
+
+        fill_field_value(check, "Time", er.fields["TransactTime"],
+                         value.message_value.fields["MDEntryTime"])
+
+        time = er.fields["TransactTime"].simple_value
+        if time[11::] == value.message_value.fields["MDEntryTime"].simple_value:
+            check[2]["Time"] = "PASSED"
+
+        table = TableComponent(['Name', 'ER Value', 'MDR Value', 'Status'])
+        field = check[0].keys()
+        for value in field:
+            table.add_row(value, check[0][value], check[1][value], check[2][value])
+
+        #event_message = f'mdr: {mdr.fields["MDReqID"].simple_value} vs er: {er.fields["ClOrdID"].simple_value}'
+        body = EventUtils.create_event_body(table)
         attach_ids = [msg.proto_message.metadata.id for msg in messages]
+        status = EventStatus.SUCCESS if ("FAILED" in check[2].values()) is False else EventStatus.FAILED
         return EventUtils.create_event(name=f"Match by '{ReconMessage.get_info(info_for_name)}'",
-                                       status=verification_component.status,
+                                       status=status,
                                        attached_message_ids=attach_ids,
                                        body=body)
