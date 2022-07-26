5526ab9b48bf1ed8dfd2684470dead2db3995a2d
alexander.zhilov <alexander.zhilov@exactprosystems.com>
2022-07-26 14:10:43 +0000

    INIT branch for new demo

--- src/rules/Match_NewOrderSingle_with_ExecutionReport.py
+++ src/rules/Match_NewOrderSingle_with_ExecutionReport.py
@@ -50,7 +50,7 @@ class Rule(rule.Rule):
         message_type: str = message.proto_message.metadata.message_type
         session_alias = message.proto_message.metadata.id.connection_id.session_alias
         direction = message.proto_message.metadata.id.direction
-        if session_alias not in self.config.keys() or message_type not in ['ExecutionReport', 'NewOrderSingle']:
+        if session_alias not in self.config or message_type not in ['ExecutionReport', 'NewOrderSingle']:
             return
         if message_type == 'ExecutionReport' and (direction != Direction.FIRST
                                                   or message.proto_message.fields['ExecType'].simple_value != '0'):
