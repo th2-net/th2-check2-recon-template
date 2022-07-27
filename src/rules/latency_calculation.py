c0c8591c0e04a2255aceb46ec0c837a631416a76
Sergey Raevsky <sergey.raevsky@exactprosystems.com>
2022-07-27 14:40:30 +0400

    Rule configuration enrichment

--- src/rules/latency_calculation.py
+++ src/rules/latency_calculation.py
@@ -20,7 +20,7 @@ from typing import Optional, Any, Dict
 from th2_check2_recon import rule
 from th2_check2_recon.common import EventUtils, TableComponent, MessageUtils
 from th2_check2_recon.reconcommon import ReconMessage, MessageGroupType
-from th2_grpc_common.common_pb2 import Event, EventStatus, Message, MessageID, ConnectionID
+from th2_grpc_common.common_pb2 import Event, EventStatus, MessageID, ConnectionID
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +34,7 @@ class LatencyCalculationMode(Enum):
     TIMESTAMP = 'Timestamp'
     SENDING_TIME = 'SendingTime'
     RESPONSE_TIME = 'ResponseTime'
+    CUSTOM = 'Custom'
 
     @classmethod
     def _missing_(cls, value: object) -> Any:
@@ -107,36 +108,76 @@ class Rule(rule.Rule):
     def configure(self, configuration: dict):
         self.request_message_types = configuration.get('RequestMessageTypes', ['NewOrderSingle'])
         self.request_message_session_aliases = configuration.get('RequestMessageSessionAliases', [])
+        self.request_hash_field = configuration.get('RequestHashField', 'ClOrdID')
+        self.request_time = configuration.get('RequestTime', 'TransactTime')
 
         self.response_message_types = configuration.get('ResponseMessageTypes', ['ExecutionReport'])
         self.response_message_session_aliases = configuration.get('ResponseMessageSessionAliases', [])
+        self.response_hash_field = configuration.get('ResponseHashField', 'ClOrdID')
+        self.response_time = configuration.get('ResponseTime', 'TransactTime')
 
-        self.hash_field = configuration.get('HashField', 'ClOrdID')
         self.mode = LatencyCalculationMode(configuration.get('Mode', 'Timestamp'))
 
         self.latency_info = configuration.get('LatencyInfo', 'Latency')
 
-    def group(self, message: ReconMessage, attributes: tuple, *args, **kwargs):
+    def determine_message(self, message: ReconMessage):
         message_type: str = message.proto_message['metadata']['message_type']
         session_alias: str = message.proto_message['metadata']['session_alias']
 
         if message_type in self.request_message_types and \
                 (len(self.request_message_session_aliases) == 0 or
                  session_alias in self.request_message_session_aliases):
-            message.group_id = Group.REQUEST
+            return Group.REQUEST
         elif message_type in self.response_message_types and \
                 (len(self.response_message_session_aliases) == 0 or
                  session_alias in self.response_message_session_aliases):
+            return Group.RESPONSE
+
+    def latency_custom(self, response_message: Dict[str, Any], request_message: Dict[str, Any]):
+
+        request_time = request_message['fields'][self.request_time]
+        response_time = response_message['fields'][self.response_time]
+
+        try:
+            request_time = datetime.strptime(request_time, '%Y-%m-%dT%H:%M:%S.%f')
+        except ValueError:
+            try:
+                request_time = datetime.strptime(request_time, '%Y-%m-%dT%H:%M:%S')
+            except ValueError:
+                request_time = datetime.strptime(request_time, '%Y-%m-%dT%H:%M')
+
+        try:
+            response_time = datetime.strptime(response_time, '%Y-%m-%dT%H:%M:%S.%f')
+        except ValueError:
+            try:
+                response_time = datetime.strptime(response_time, '%Y-%m-%dT%H:%M:%S')
+            except ValueError:
+                response_time = datetime.strptime(response_time, '%Y-%m-%dT%H:%M')
+
+        latency = (response_time - request_time) / timedelta(microseconds=1)
+        return latency
+
+    def group(self, message: ReconMessage, attributes: tuple, *args, **kwargs):
+        group = self.determine_message(message)
+
+        if group == Group.REQUEST:
+            message.group_id = Group.REQUEST
+        elif group == Group.RESPONSE:
             message.group_id = Group.RESPONSE
             
     def hash(self, message: ReconMessage, attributes: tuple, *args, **kwargs):
-        hash_field = message.proto_message['fields'][self.hash_field]
-        message.hash = hash(hash_field)
-        message.hash_info[self.hash_field] = hash_field
+        group = self.determine_message(message)
+
+        if group == Group.REQUEST:
+            hash_field = message.proto_message['fields'][self.request_hash_field]
+            message.hash = hash(hash_field)
+            message.hash_info[self.request_hash_field] = hash_field
+        elif group == Group.RESPONSE:
+            hash_field = message.proto_message['fields'][self.response_hash_field]
+            message.hash = hash(hash_field)
+            message.hash_info[self.response_hash_field] = hash_field
 
     def check(self, messages: [ReconMessage], *args, **kwargs) -> Event:
-
-        hash_field = messages[0].proto_message['fields'][self.hash_field]
 
         request_message: Optional[Dict[str, Any]] = None
         request_message_type = ''
@@ -146,18 +187,13 @@ class Rule(rule.Rule):
         message: ReconMessage
         for message in messages:
             proto_message = message.proto_message
-            message_type: str = message.proto_message['metadata']['message_type']
-            session_alias: str = message.proto_message['metadata']['session_alias']
+            group = self.determine_message(message)
+            message_type: str = proto_message['metadata']['message_type']
 
-            if message_type in self.request_message_types and \
-                    (len(self.request_message_session_aliases) == 0 or
-                     session_alias in self.request_message_session_aliases):
+            if group == Group.REQUEST:
                 request_message = proto_message
                 request_message_type = message_type
-
-            elif message_type in self.response_message_types and \
-                    (len(self.response_message_session_aliases) == 0 or
-                     session_alias in self.response_message_session_aliases):
+            elif group == Group.RESPONSE:
                 response_message = proto_message
                 response_message_type = message_type
 
@@ -165,21 +201,26 @@ class Rule(rule.Rule):
             latency = latency_by_sending_time(response_message, request_message)
         elif self.mode == LatencyCalculationMode.RESPONSE_TIME:
             latency = latency_response_time(response_message, request_message)
+        elif self.mode == LatencyCalculationMode.CUSTOM:
+            latency = self.latency_custom(response_message, request_message)
         else:
             latency = latency_by_timestamp(response_message, request_message)
 
         request_timestamp = str(request_message['metadata']['timestamp'].ToDatetime())
+        request_hash_field = request_message['fields'][self.request_hash_field]
+        response_hash_field = response_message['fields'][self.response_hash_field]
 
         table = TableComponent(['Name', 'Value'])
         table.add_row('Message Type', request_message_type)
         table.add_row('Timestamp', request_timestamp)
-        # table.add_row('Response Message Type', response_message_type)
-        table.add_row(f'{self.hash_field}', hash_field)
+        table.add_row(f'{self.request_hash_field}', request_hash_field)
         table.add_row('Latency in us', latency)
 
-        logger.debug('Rule: %s. Latency between %s and %s message with %s = %s is equal to %s',
-                     self.get_name(), request_message_type, response_message_type,
-                     self.hash_field, hash_field, latency)
+        logger.debug('Rule: %s. Latency between %s with %s = %s and %s with %s = %s is equal to %s',
+                     self.get_name(),
+                     request_message_type, self.request_hash_field, request_hash_field,
+                     response_message_type, self.response_hash_field, response_hash_field,
+                     latency)
 
         body = EventUtils.create_event_body(table)
 
@@ -189,7 +230,8 @@ class Rule(rule.Rule):
                       for msg in messages]
 
         return EventUtils.create_event(name=f'{self.latency_info} between messages with '
-                                            f'{self.hash_field} = {hash_field}',
+                                            f'{self.request_hash_field} = {request_hash_field} and'
+                                            f'{self.response_hash_field} = {response_hash_field}',
                                        status=EventStatus.SUCCESS,
                                        attached_message_ids=attach_ids,
                                        body=body)
